2905be7c43841b1cda2bf9bf4745ee3639575669
safety0ff <safety0ff@users.sourceforge.net>
2011-06-15 20:12:52 +0000

    Disable the visual debuging code for now.

--- src/display3d.cpp
+++ src/display3d.cpp
@@ -713,6 +713,7 @@ void draw3DScene( void )
 	displayTerrain();
 
 	// BEGIN - DELETE ME DEBUG
+#if 0
 	pie_SetRendMode(REND_OPAQUE);
 	pie_SetTexturePage(TEXPAGE_NONE);
 	pie_SetAlphaTest(false);
@@ -742,6 +743,7 @@ void draw3DScene( void )
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	pie_SetDepthBufferStatus(DEPTH_CMP_LEQ_WRT_ON);
 	glEnable(GL_CULL_FACE);
+#endif
 	// END - DELETE ME DEBUG
 
 	pie_BeginInterface();
@@ -1112,6 +1114,7 @@ static void drawTiles(iView *player)
 	drawWater();
 
 	// BEGIN - DELETE ME DEBUG
+#if 0
 	glPointSize(10.f);
 	glColor3f(1.f, 1.f, 0.f);
 	pie_SetDepthBufferStatus(DEPTH_CMP_ALWAYS_WRT_OFF);
@@ -1122,6 +1125,7 @@ static void drawTiles(iView *player)
 	glEnd();
 	glColor3f(1.f, 1.f, 1.f);
 	pie_SetDepthBufferStatus(DEPTH_CMP_LEQ_WRT_ON);
+#endif
 	// END - DELETE ME DEBUG
 
 	// and to the warzone modelview transform
